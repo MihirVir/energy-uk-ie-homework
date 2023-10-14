30f950658743ee474c8f1df7690aecec5dad9c94
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2023-10-15 00:41:29 +0100

    routes setup

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
+const homeRoute = require("./routes/index");
 const app = express();
 
 
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // applying routes
-
+app.use("/home", homeRoute);
 
 // spinning up the server
 app.listen(process.env.PORT || 5000, () => {
